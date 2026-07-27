9d4cf45ecf6e005ee2a2eec07a0256d1bd6701e7
linhlell-15 <le224l@mtholyoke.edum>
2026-07-26 22:59:08 -0400

    update redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,6 +6,10 @@ cd ~/portfolio-linhle
 
 git fetch && git reset origin/main --hard
 
+docker compose -f docker-compose.prod.yml down
+
+docker compose -f docker-compose.prod.yml up -d --build
+
 source python3-virtualvenv/bin/activate
 pip install -r requirements.txt
 
